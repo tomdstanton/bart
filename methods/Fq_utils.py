40d80fdbf60871a019f8d071041d60e76ba171a7
tomdstanton <tomdstanton@gmail.com>
2021-03-05 15:58:07 +0000

    pushing changes for windows pc

---
+++ methods/Fq_utils.py
@@ -0,0 +1,80 @@
+import mmap
+import re
+import contextlib
+import io
+
+class FqUtils():
+
+    reads_file = '/home/tom/PycharmProjects/genefinda/test/1543722_R1.fastq'
+    def try_open(reads_file):
+        fp = None
+        try:
+            import gzip
+            fp = gzip.open(reads_file)
+            ln = fp.read(2)  # read arbitrary bytes to check if gzipped
+            fp.close()  # if we're here, it's definitely gzip
+            fp = gzip.open(reads_file)
+        except Exception as e:
+            if fp: fp.close()
+            fp = open(reads_file)
+        return fp
+
+    # auxiliary function
+    def get_read_length(list_reads):
+        read_lengths = []
+        for reads_file in list_reads:
+            fp = try_open(reads_file)
+            # read the 1000 first reads
+            from methods.readfq import readfq
+            read_count = 0
+            for name, seq, qual in readfq(fp):
+                read_count += 1
+                read_lengths += [len(seq)]
+                if read_count > 999:
+                    break
+            fp.close()
+
+        # so, on CEA cluster, loading numpy forced minia to run on a single thread.
+        # so let's not use numpy here. days of debugging to get that.
+        import math
+        def percentile(data, percentile):
+            size = len(data)
+            return sorted(data)[int(math.ceil((size * percentile) / 100)) - 1]
+
+        if len(read_lengths) == 0:
+            print("Warning: couldn't detect max read length. Are you sure the input is correct?")
+            exit("")
+
+        estimated_max_read_length = percentile(read_lengths, 90)
+        print("Setting maximum kmer length to: " + str(
+            estimated_max_read_length) + " bp")  # based on the 90 percentile of 1000 first reads lengths of each input file
+        return estimated_max_read_length
+
+    #def gc_calc(self):
+
+    test = '/home/tom/PycharmProjects/genefinda/test/1543722_R1.fastq'
+
+    def grep(pattern, file_path):
+        with io.open(file_path, "r", encoding="utf-8") as f:
+            with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
+                for match in pattern.findall(m):
+                    return(print(match))
+
+    grep(re.compile(b'@'), test)
+
+    with io.open(test, "r", encoding="utf-8") as f:
+        with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
+            for line in m:
+                print(len(line))
+
+
+import mmap
+import gzip
+
+filename = "a.gz"
+handle = open(filename, "rb")
+mapped = mmap.mmap(handle.fileno(), 0, access=mmap.ACCESS_READ)
+gzfile = gzip.GzipFile(mode="r", fileobj=mapped)
+
+print gzfile.read()
+
--- tools/run_kma.py
+++ tools/run_kma.py
@@ -9,3 +9,9 @@
 # ./kma/kma index -i test/ab_mlst_pasteur/* -o test/ab_mlst
 #
 # ./kma/kma -ipe test/*R1* test/*R2* -ID 80 -o kma_test -t_db test/ab_mlst
+
+# ./tools/kma/kma -ipe test/1543722_R1_001.fastq.gz test/1543722_R2_001.fastq.gz -t_db amr_genes -o testttt -na -nf -ID 50 -bc90 -mct 0 -t 4
+
+# cat db/genes/Acinetobacter\ baumannii/pi* | ./tools/kma/kma index -i -- -t_db amr_genes
+
+# http://backup.mediterranee-infection.com/arkotheque/client/ihumed/_depot_arko/articles/2042/arg-annot-v4-nt-may2018_doc.fasta
