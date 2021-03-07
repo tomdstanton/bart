2ac95be6099ccc4b005bba94da086c4a0a9669a1
tomdstanton <tomdstanton@gmail.com>
2021-03-07 10:57:12 +0000

    implementation of finch

--- genefinda/methods/Fq_utils.py
+++ genefinda/methods/Fq_utils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import mmap
 import re
 import contextlib
---
+++ genefinda/tools/finch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
