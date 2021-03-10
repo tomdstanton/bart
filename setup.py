78024321a01bc71a3a0d0ba2c8f6f6f1d71e7025
tomdstanton <tomdstanton@gmail.com>
2021-03-10 16:40:51 +0000

    pre-packaging

--- setup.py
+++ setup.py
@@ -1,25 +1,35 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+with open("README.md", "r", encoding="utf-8") as rm:
+    long_description = rm.read()
+
+setup(
+    name="genefinda",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Tom Stanton",
+    author_email="tomdstanton@gmail.com",
+    description="Quickly find MLST profile and genes of interest in bacterial short reads",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tomdstanton/genefinda",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/tomdstanton/genefinda/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    include_package_data = True,
+    package_data={'genefinda': ['genefinda/data/']},
+    scripts=['genefinda/genefinda'],
+    author_email='T.D.Stanton@sms.ed.ac.uk',
+    keywords='microbial genomics amr virulence',
+    install_requires=requirements,
+    zip_safe=False
 )
